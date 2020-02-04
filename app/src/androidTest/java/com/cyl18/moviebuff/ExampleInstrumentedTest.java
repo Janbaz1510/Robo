161a3dda2eeef2b6a99b4ad67316ce258e466016
janbaz <janbazahmad9807@gmail.com>
2020-02-03 16:02:42 -0800

    Movie Buff

--- app/src/androidTest/java/com/cyl18/moviebuff/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cyl18/moviebuff/ExampleInstrumentedTest.java
@@ -3,7 +3,7 @@ package com.cyl18.moviebuff;
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
